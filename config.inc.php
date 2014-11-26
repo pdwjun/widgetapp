6a26706b6bfaa0f87d062255ecc6ca50af64c38a
pdwjun <pdwjun@gmail.com>
2014-11-26 14:56:02 +0800

    mysql user pwd

--- config.inc.php
+++ config.inc.php
@@ -3,8 +3,8 @@
   	'DB_TYPE'               => 'mysql',     // 数据库类型
     'DB_HOST'               => 'localhost', // 服务器地址
     'DB_NAME'               => 'daiyun',          // 数据库名
-    'DB_USER'               => 'daiyun',      // 用户名
-    'DB_PWD'                => 'daiyun365',          // 密码
+    'DB_USER'               => 'jason',      // 用户名
+    'DB_PWD'                => 'lrc207107',          // 密码
     'DB_PORT'               => '3306',        // 端口
     'DB_PREFIX'             => 'hh85_',    // 数据库表前缀
     'DB_CHARSET'            => 'utf8',      // 数据库编码默认采用utf8
